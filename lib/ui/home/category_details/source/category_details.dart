222665b3c517ac049b2b879c3e45757810c80893
monamohgh <mny990127@gmail.com>
2026-08-29 13:57:53 +0300

    initialize feat mvvm-provider

--- lib/ui/home/category_details/source/category_details.dart
+++ lib/ui/home/category_details/source/category_details.dart
@@ -3,11 +3,15 @@ import 'package:news_app/api/api_manager.dart';
 import 'package:news_app/api/model/category/category.dart';
 import 'package:news_app/api/model/source/source_response.dart';
 import 'package:news_app/l10n/app_localizations.dart';
+import 'package:news_app/ui/home/category_details/source/source_view_model.dart';
 import 'package:news_app/ui/home/category_details/source/source_widget.dart';
 import 'package:news_app/ui/home/widget/main_error_widget.dart';
 import 'package:news_app/ui/home/widget/main_loading_widget.dart';
-class  CategoryDetails extends StatefulWidget {
-  const  CategoryDetails({super.key,required this.category});
+import 'package:provider/provider.dart';
+
+class CategoryDetails extends StatefulWidget {
+  const CategoryDetails({super.key, required this.category});
+
   final Category category;
 
   @override
@@ -15,50 +19,81 @@ class  CategoryDetails extends StatefulWidget {
 }
 
 class _CategoryDetailsState extends State<CategoryDetails> {
+  ///view=>observer
+  ///every view(UI=>_CategoryDetailsState) know its view model(SourceViewModel)=>have object from the viewmodel
+  SourceViewModel viewModel = SourceViewModel();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    viewModel.getSources(widget.category.id);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<SourceResponse>(
-        future: ApiManager.getSources(widget.category.id),
-        builder: (context, snapshot){
-          //todo:loading
-          if(snapshot.connectionState==ConnectionState.waiting){
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => viewModel,
+      ///Consumer=>listen to the view model
+      child: Consumer<SourceViewModel>(
+        builder: (context, viewModel, child) {
+          if (viewModel.isLoading) {
+            //todo:loading
             return MainLoadingWidget();
-          }else if(snapshot.hasError){
+          } else if (viewModel.errorMessage != null) {
             //todo:error
             return MainErrorWidget(
-                errorMessage: AppLocalizations.of(context)!.something_went_wrong,
-                onPressed: (){
-                  //todo:try again
-                    ApiManager.getSources(widget.category.id);
-                    setState(() {
-
-                    });
-                }
+              errorMessage: viewModel.errorMessage!,
+              onPressed: () {
+                //todo:try again
+                viewModel.getSources(widget.category.id);
+              },
             );
+          }else if(viewModel.sourcesList==null){
+            return MainLoadingWidget();
           }else{
-            //todo:server has response with data
-            //todo:response=>success,error
-            if(snapshot.data?.status!='ok'){
-              //todo:server=>response=>error
-              return MainErrorWidget(
-                  errorMessage: snapshot.data!.message!,
-                  onPressed: (){
-                    //todo:try again
-                    ApiManager.getSources(widget.category.id);
-                    setState(() {
-
-                    });
-
-                  }
-              );
-            }else{
-              //todo:server=>response=>success
-              var sourcesList=snapshot.data?.sources??[];
-              return SourceWidget(sourcesList: sourcesList);
-
-            }
+            //todo:success
+                    var sourcesList = viewModel.sourcesList ??[];
+                    return SourceWidget(sourcesList: sourcesList);
           }
         },
+      ),
+      // FutureBuilder<SourceResponse>(
+      //   future: ApiManager.getSources(widget.category.id),
+      //   builder: (context, snapshot) {
+      //     //todo:loading
+      //     if (snapshot.connectionState == ConnectionState.waiting) {
+      //       return MainLoadingWidget();
+      //     } else if (snapshot.hasError) {
+      //       //todo:error
+      //       return MainErrorWidget(
+      //         errorMessage: AppLocalizations.of(context)!.something_went_wrong,
+      //         onPressed: () {
+      //           //todo:try again
+      //           ApiManager.getSources(widget.category.id);
+      //           setState(() {});
+      //         },
+      //       );
+      //     } else {
+      //       //todo:server has response with data
+      //       //todo:response=>success,error
+      //       if (snapshot.data?.status != 'ok') {
+      //         //todo:server=>response=>error
+      //         return MainErrorWidget(
+      //           errorMessage: snapshot.data!.message!,
+      //           onPressed: () {
+      //             //todo:try again
+      //             ApiManager.getSources(widget.category.id);
+      //             setState(() {});
+      //           },
+      //         );
+      //       } else {
+      //         //todo:server=>response=>success
+      //         var sourcesList = snapshot.data?.sources ?? [];
+      //         return SourceWidget(sourcesList: sourcesList);
+      //       }
+      //     }
+      //   },
+      // ),
     );
   }
 }
---
+++ lib/ui/home/category_details/source/source_view_model.dart
@@ -0,0 +1,36 @@
+//todo:viewModel => state management(observable => have data )
+import 'package:flutter/widgets.dart';
+import 'package:news_app/api/api_manager.dart';
+import 'package:news_app/api/model/source/source.dart';
+
+class SourceViewModel extends ChangeNotifier{
+  //todo:view model:hold data/handel logic
+  /// the data
+  List<Source>? sourcesList;
+  String? errorMessage;
+  bool isLoading =false;
+  /// the logic
+  void getSources(String categoryId)async{
+try{
+  //todo:loading
+  isLoading=true;
+  var sourceResponse=await ApiManager.getSources(categoryId);
+  if(sourceResponse.status=='error'){
+    //todo:server response=>error
+    isLoading=false;
+     errorMessage=sourceResponse.message!;
+  }else{
+    //todo:server response=>success=>data
+    isLoading=false;
+    sourcesList=sourceResponse.sources!;
+  }
+}catch(e){
+  ///error from the client not from the server
+  isLoading=false;
+errorMessage=e.toString();
+}
+notifyListeners();
+  }
+
+
+}
